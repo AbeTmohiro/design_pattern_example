752305d24de7239c77156d1c651ebca7ec2e8de5
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2021-01-05 17:02:39 +0900

    Update ads_controller.rb
    配列の定義と取得をPrivateメソッドに委譲
    sampleメソッドでランダムに値を取得

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,8 +1,19 @@
 class AdsController < ApplicationController
   def index
-    ad_types = ["gif","image","movie"]
+    choose_ad_types
+    choose_image_counts
+  end
+
+  private
+  def choose_ad_types
+    ad_types = ["gif", "image", "movie"]
+    # sampleメソッドを使って配列の中身をランダムに取得
     @ad_type = ad_types.sample
-    image_counts = [1,2]
+  end
+
+  def choose_image_counts
+    image_counts = [1, 2]
+    # sampleメソッドを使って配列の中身をランダムに取得
     @image_count = image_counts.sample
   end
 end
