f2d047b34baf4b34b6d559f19e6fc78b50548208
AbeTmohiro <tomohiro.abe@di-v.co.jp>
2021-01-19 17:21:05 +0900

    add

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,10 @@
-module AdsHelper
-  def ad(ad_type, image_count)
+module PostsHelper
+  
+  attr_reader :ad_type, :image_count
+  def ad
+    # ad_type = RandomValue.new.choose_ad_types
+    # image_count = RandomValue.new.choose_image_counts
+    define_attribute
     if ad_type == "gif"
       gif = Gif.new
       gif.return_html(ad_type, image_count)
@@ -11,9 +16,32 @@ module AdsHelper
       movie.return_html(ad_type, image_count)
     end
   end
+
+  private
+
+  def define_attribute
+    ad_types = ["gif", "image", "movie"]
+    image_counts = [1, 2]
+    @ad_type = ad_types.sample
+    @image_count = image_counts.sample
+  end
 end
 
 # -------------- #
+
+# class RandomValue
+  # def choose_ad_types
+  #   ad_types = ["gif", "image", "movie"]
+  #   # sampleメソッドを使って配列の中身をランダムに取得
+  #   ad_types.sample
+  # end
+
+  # def choose_image_counts
+  #   image_counts = [1, 2]
+  #   # sampleメソッドを使って配列の中身をランダムに取得
+  #   image_counts.sample
+  # end
+# end
 
 class AdTemplate
   def initialize
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'ads/index'
-  root to: 'ads#index' 
+  get 'posts/index'
+  root to: 'posts#index' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
